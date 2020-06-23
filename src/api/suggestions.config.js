c34e0f12de6b4723ce0d2dab7f3860c9b059ba26
Tiago De Pina <tiagopina20014@gmail..com>
2020-06-23 03:30:51 +0100

    hustling with suggestions

---
+++ src/api/suggestions.config.js
@@ -0,0 +1,58 @@
+import API_URL from "./config";
+import axios from "axios";
+
+const HTTP = axios.create({
+  baseURL: API_URL
+});
+
+let headers = {
+  "Access-Control-Allow-Origin": "*"
+};
+
+const suggestionConfig = {
+  // *"updates" suggestions
+  async updateSuggestion(id_suggestion, id_status) {
+    try {
+      const response = await HTTP.put(
+        `${API_URL}/suggestions/update/${id_suggestion}`,
+        {
+          id_status: id_status
+        },
+        {
+          headers
+        }
+      );
+      return {
+        resStatus: response.status
+      };
+    } catch (err) {
+      localStorage.setItem("error", JSON.stringify(err.response.data.error));
+      return err;
+    }
+  },
+
+  //*Get all not reader notification by user id
+  async getAllSuggestions() {
+    try {
+      const response = await HTTP.get(`${API_URL}//suggestions`, {
+        headers
+      });
+      if (response.status == 200) {
+        return {
+          suggestions: response.data,
+          resStatus: response.status
+        };
+      } else {
+        return {
+          suggestions: [],
+          resStatus: response.status
+        };
+      }
+    } catch (err) {
+      localStorage.setItem("error", JSON.stringify(err.response.data.error));
+      return err;
+    }
+  }
+};
+
+export default suggestionConfig;

2d40fd60ed7c058b80f85744bdd4972dfcaa9351
Nuno Sousa <9180579@esmad.ipp.pt>
2020-06-25 09:46:29 +0100

    Hustle

---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="container position-relative d-flex justify-content-center align-items-center"
+  >
+    <div class="row position-absolute">
+      <div class="col-12">
+        <h1
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="1200"
+          data-aos-once="true"
+          data-aos-easing="ease"
+        >
+          Error
+        </h1>
+        <hr
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="1500"
+          data-aos-once="true"
+          data-aos-easing="ease"
+        />
+        <h4
+          class="pt-4"
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-delay="1800"
+          data-aos-once="true"
+          data-aos-easing="ease"
+        >
+          Please choose a section that follows the pages we have!
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 76.5vh;
+  font-family: "Geared Slab";
+  h1 {
+    font-size: 10em;
+  }
+  hr {
+    width: 45vw;
+    background: #f0f0f0;
+  }
+  h4 {
+    color: #e9d496;
+    font-weight: 500;
+  }
+}
+</style>
